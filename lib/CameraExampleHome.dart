21cde8dc98ef19922a167311394a5ea979746e46
Viet Nguyen <>
2026-03-02 09:25:20 +0700

    detect image classification success

--- lib/CameraExampleHome.dart
+++ lib/CameraExampleHome.dart
@@ -6,6 +6,7 @@ import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'dart:io';
+import 'package:logger/logger.dart';
 
 class CameraExampleHome extends StatefulWidget {
   const CameraExampleHome({super.key, required this.cameras});
@@ -26,6 +27,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
   String result = "results to be shown here...";
   late ImageLabeler imageLabeler;
   int _cameraIndex = 0;
+  final Logger logger = Logger();
 
   // A boolean flag to track when the camera is fully initialized and ready for display
   bool _isCameraInitialized = false;
@@ -51,21 +53,14 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
           : ImageFormatGroup.bgra8888,
     );
 
-    final _orientations = {
-      DeviceOrientation.portraitUp: 0,
-      DeviceOrientation.landscapeLeft: 90,
-      DeviceOrientation.portraitDown: 180,
-      DeviceOrientation.landscapeRight: 270,
-    };
-
     try {
       // 2. Await the initialization
       await controller.initialize().then((_) {
         if (!mounted) return;
-        controller.startImageStream((CameraImage img) {
+        controller.startImageStream((image) {
           if (!isBusy) {
             isBusy = true;
-            img = img;
+            img = image;
             doImageLabeling();
           }
           ;
@@ -83,8 +78,84 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
     }
   }
 
+  final _orientations = {
+    DeviceOrientation.portraitUp: 0,
+    DeviceOrientation.landscapeLeft: 90,
+    DeviceOrientation.portraitDown: 180,
+    DeviceOrientation.landscapeRight: 270,
+  };
+
+  InputImage? _inputImageFromCameraImage(CameraImage image) {
+    // get image rotation
+    // it is used in android to convert the InputImage from Dart to Java
+    // `rotation` is not used in iOS to convert the InputImage from Dart to Obj-C
+    // in both platforms `rotation` and `camera.lensDirection` can be used to compensate `x` and `y` coordinates on a canvas
+    final camera = widget.cameras[0];
+    final sensorOrientation = camera.sensorOrientation;
+    InputImageRotation? rotation;
+    if (Platform.isIOS) {
+      rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+    } else if (Platform.isAndroid) {
+      var rotationCompensation =
+          _orientations[controller!.value.deviceOrientation];
+      if (rotationCompensation == null) return null;
+      if (camera.lensDirection == CameraLensDirection.front) {
+        // front-facing
+        rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+      } else {
+        // back-facing
+        rotationCompensation =
+            (sensorOrientation - rotationCompensation + 360) % 360;
+      }
+      rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
+    }
+    if (rotation == null) return null;
+
+    // get image format
+    final format = InputImageFormatValue.fromRawValue(image.format.raw);
+    // validate format depending on platform
+    // only supported formats:
+    // * nv21 for Android
+    // * bgra8888 for iOS
+    if (format == null ||
+        (Platform.isAndroid && format != InputImageFormat.nv21) ||
+        (Platform.isIOS && format != InputImageFormat.bgra8888))
+      return null;
+
+    // since format is constraint to nv21 or bgra8888, both only have one plane
+    if (image.planes.length != 1) return null;
+    final plane = image.planes.first;
+
+    // compose InputImage using bytes
+    return InputImage.fromBytes(
+      bytes: plane.bytes,
+      metadata: InputImageMetadata(
+        size: Size(image.width.toDouble(), image.height.toDouble()),
+        rotation: rotation, // used only in Android
+        format: format, // used only in iOS
+        bytesPerRow: plane.bytesPerRow, // used only in iOS
+      ),
+    );
+  }
+
   doImageLabeling() async {
+    logger.d("Logger is working! doImageLabeling()");
     result = "";
+
+    InputImage? inputImage = _inputImageFromCameraImage(img!);
+    final List<ImageLabel> labels = await imageLabeler.processImage(
+      inputImage!,
+    );
+
+    logger.d("Logger is working! doImageLabeling()");
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      result += text + "  " + confidence.toStringAsFixed(2) + "\n";
+    }
+    logger.d("Logger is working! result: $result");
     setState(() {
       result;
       isBusy = false;
